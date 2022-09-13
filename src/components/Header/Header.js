1de904d002d35158a950c87c762d8b3c8ee37d31
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-13 16:51:09 -0700

    added css to header, added more css to main, zodiac, footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Header.css';
 
 export default function Header() {
   return (
-    <div></div>
+    <header>Alchemy Deep Science</header>
   );
 }
--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -5,7 +5,7 @@ export default function Zodiac({ name, dates }) {
     <div className="zodiac">
       <img alt={name} src={`${process.env.PUBLIC_URL}/zodiac_signs/${name}.png`} />
       <span className="name">{name}</span>
-      <span>{dates}</span>
+      <span className="dates">{dates}</span>
     </div>
   );
 }
